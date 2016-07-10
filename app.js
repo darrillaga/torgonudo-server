fd6fd09bef45aca8caea293aceafd34131eebedc
Damián Arrillaga <damian.arrillaga@gmail.com>
2016-07-10 19:51:58 -0300

    Add express

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
-const http         = require('http'),
-      fs           = require('fs'),
-      path         = require('path'),
-      contentTypes = require('./utils/content-types'),
-      sysInfo      = require('./utils/sys-info'),
-      env          = process.env;
+const
+    express = require("express"),
+    app = express(),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override"),
+    fs = require('fs'),
+    path = require('path'),
+    contentTypes = require('./utils/content-types'),
+    sysInfo = require('./utils/sys-info'),
+    env = process.env;
 
-let server = http.createServer(function (req, res) {
-  let url = req.url;
-  if (url == '/') {
-    url += 'index.html';
-  }
 
-  // IMPORTANT: Your application HAS to respond to GET /health with status 200
-  //            for OpenShift health monitoring
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
-  if (url == '/health') {
-    res.writeHead(200);
-    res.end();
-  } else if (url == '/info/gen' || url == '/info/poll') {
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Cache-Control', 'no-cache, no-store');
-    res.end(JSON.stringify(sysInfo[url.slice(6)]()));
-  } else {
-    fs.readFile('./static' + url, function (err, data) {
-      if (err) {
-        res.writeHead(404);
-        res.end('Not found');
-      } else {
-        let ext = path.extname(url).slice(1);
-        res.setHeader('Content-Type', contentTypes[ext]);
-        if (ext === 'html') {
-          res.setHeader('Cache-Control', 'no-cache, no-store');
-        }
-        res.end(data);
+var router = express.Router();
+
+var respondStaticUrl = function(url, res) {
+  fs.readFile('./static' + url, function (err, data) {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+    } else {
+      let ext = path.extname(url).slice(1);
+      res.setHeader('Content-Type', contentTypes[ext]);
+      if (ext === 'html') {
+        res.setHeader('Cache-Control', 'no-cache, no-store');
       }
-    });
-  }
+      res.end(data);
+    }
+  });
+}
+
+router.get('/', function(req, res) {
+  respondStaticUrl("/index.html", res);
 });
 
-server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
+router.get('/health', function(req, res) {
+  res.writeHead(200);
+  res.end();
+});
+
+var infoGenPollResponse = function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'no-cache, no-store');
+  res.end(JSON.stringify(sysInfo[url.slice(6)]()));
+}
+
+router.get('/info/gen', infoGenPollResponse);
+router.get('/info/poll', infoGenPollResponse);
+
+app.use(router);
+
+app.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
